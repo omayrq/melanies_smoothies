5e5112bb3513378e881b9e603040aabe2cedec03
Umair Ashraf <120219850+omayrq@users.noreply.github.com>
2025-12-19 15:33:23 +0500

    Update streamlit_app.py
    Adding request library to fetch API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 # Try Snowflake Streamlit first, else fallback to normal Streamlit
 def get_snowflake_session():
