f1316720c0225cdbd6c2d31cc17f3b303a2148ea
Umair Ashraf <120219850+omayrq@users.noreply.github.com>
2025-12-21 10:43:55 +0500

    Update streamlit_app.py
    Uncommenting smoothiefruit API and adding search on

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,12 +48,14 @@ if ingredients_list:
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + 'Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         
-        #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")#+ fruit_chosen)
         # st.text(smoothiefroot_response)
-        #sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-        sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        
+        #sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         
     
     if st.button("Submit Order"):
